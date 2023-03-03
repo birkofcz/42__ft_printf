62b5cc1fea4e64fd9413a97389d999b63df517e8
Stepan Benes <sbenes@c1r5s6.42prague.com>
2023-03-03 17:39:55 +0100

    work bu

--- ft_puthex.c
+++ ft_puthex.c
@@ -6,7 +6,7 @@
 /*   By: sbenes <sbenes@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 16:41:35 by sbenes            #+#    #+#             */
-/*   Updated: 2023/03/03 17:18:28 by sbenes           ###   ########.fr       */
+/*   Updated: 2023/03/03 17:39:08 by sbenes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,37 +29,17 @@ int	ft_nbrlen_hex(int nb)
 	return (i);
 }
 
-// int	ft_putnbr_hex(int nb, char x)
-// {
-// 	char	*hex;
-
-// 	hex = "0123456789abcdef";
-// 	if (x == 'X')
-// 		hex = "0123456789ABCDEF";
-// 	if (nb >= 16)
-// 		ft_putnbr_hex(nb / 16, x);
-// 	write(1, &hex[nb % 16], 1);
-// 	return (ft_nbrlen_hex(nb));
-// }
-
 int	ft_putnbr_hex(int nb, char x)
 {
 	char	*hex;
-	unsigned int u_nb;
 
 	hex = "0123456789abcdef";
 	if (x == 'X')
 		hex = "0123456789ABCDEF";
-	if (nb < 0)
-	{
-		write(1, "-", 1);
-		u_nb = (unsigned int)(-(nb + 1)) + 1;
-	}
-	else
-		u_nb = (unsigned int)nb;
-	if (u_nb >= 16)
-		ft_putnbr_hex(u_nb / 16, x);
-	write(1, &hex[u_nb % 16], 1);
+	
+	if (nb >= 16)
+		ft_putnbr_hex(nb / 16, x);
+	write(1, &hex[nb % 16], 1);
 	return (ft_nbrlen_hex(nb));
 }
 
