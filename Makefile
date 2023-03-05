8e5ae563a03006592d52aa07ea09a63f998e4b9d
Stepan Benes <sbenes@c3r5s3.42prague.com>
2023-03-05 13:45:36 +0100

    work bu

--- Makefile
+++ Makefile
@@ -5,38 +5,33 @@
 #                                                     +:+ +:+         +:+      #
 #    By: sbenes <sbenes@student.42prague.com>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2023/03/04 13:51:48 by sbenes            #+#    #+#              #
-#    Updated: 2023/03/04 13:52:11 by sbenes           ###   ########.fr        #
+#    Created: 2023/03/05 11:25:26 by sbenes            #+#    #+#              #
+#    Updated: 2023/03/05 13:38:01 by sbenes           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-SRC = ft_printf.c ft_putchar.c ft_putstr.c ft_putnbr.c ft_putptr.c ft_puthex.c ft_putunsigned.c
+#compilation
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+AR = ar rcs
 
-INC = ft_printf.h
+#sources
+SRC = ft_printf.c ft_putchar.c ft_putstr.c ft_putnbr.c ft_putptr.c ft_puthex.c ft_putunsigned.c
 OBJ = $(SRC:.c=.o)
 NAME = libftprintf.a
+
+#clean
 RM = rm -f
-CFLAGS = -Wall -Wextra -Werror
-LIBC = ar rc
-LIBFT = ./libft/libft.a
 
-.c.o:
-	cc ${CFLAGS} -c $< -o ${<:.c=.o} -I ${INC}
+all:			$(NAME)
 
-${NAME}: ${OBJ}
-	make -C ./libft
-	cp ./libft/libft.a ${NAME}
-	${LIBC} ${NAME} ${OBJ}
-
-all: ${NAME}
+$(NAME):		$(OBJ)
+				$(AR) $(NAME) $(OBJ)
 
 clean:
-	${MAKE} clean -C ./libft
-	${RM} ${OBJ}
-	${RM} ${BONUS_O}
+			$(RM) $(OBJ)
 
-fclean:	clean
-	${MAKE} fclean -C ./libft
-	${RM} ${NAME}
+fclean:		clean
+			$(RM) $(NAME)
 
-re: fclean all
+re:			fclean all
--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: sbenes <sbenes@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 15:29:04 by sbenes            #+#    #+#             */
-/*   Updated: 2023/03/04 17:15:48 by sbenes           ###   ########.fr       */
+/*   Updated: 2023/03/05 11:17:49 by sbenes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <limits.h>
 #include "ft_printf.h"
 
 /* TEST MAIN FOR FT_PRINTF PROJECT */
@@ -46,14 +47,33 @@ int	main(void)
 
 	/*STRING SET*/
 	char *s = "Yeeehaw! It's printing my string!";
+	char *s1 = NULL;
 
 	ft_printf("\n\033[33m[string test %%s] ------\033[0m\n\n");
 
 	nc_printed = printf("\033[34mPRINTF\033[0m char *s =\033[34m %s \033[0m\n", s);
 	ft_printf("%d printed.\n", nc_printed);
 	nc_printed = ft_printf("\033[32mFT_PRINTF\033[0m char *s =\033[32m %s \033[0m\n",s);
+	ft_printf("%d printed.\n\n", nc_printed - 3);
+
+	nc_printed = printf("\033[34mPRINTF\033[0m char *s1 = NULL:\033[34m %s \033[0m\n", s1);
+	ft_printf("%d printed.\n", nc_printed);
+	nc_printed = ft_printf("\033[32mFT_PRINTF\033[0m char *s1 = NULL:\033[32m %s \033[0m\n",s1);
 	ft_printf("%d printed.\n", nc_printed - 3);
 
+	/*DECIMAL AND INT SET*/
+
+	ft_printf("\n\033[33m[decimal and integer test %%d or %%i] ------\033[0m\n\n");
+
+	nc_printed = printf("\033[34mPRINTF\033[0m int 12345 =\033[34m %d \033[0m\n", 12345);
+	ft_printf("%d printed.\n", nc_printed);
+	nc_printed = ft_printf("\033[32mFT_PRINTF\033[0m int 12345 =\033[32m %d \033[0m\n",12345);
+	ft_printf("%d printed.\n", nc_printed - 3);
+
+	nc_printed = printf("\033[34mPRINTF\033[0m int -12345\033[34m %d \033[0m\n", -12345);
+	ft_printf("%d printed.\n", nc_printed);
+	nc_printed = ft_printf("\033[32mFT_PRINTF\033[0m int -12345\033[32m %d \033[0m\n", -12345);
+	ft_printf("%d printed.\n", nc_printed - 3);
 
 	return (0);
 }
